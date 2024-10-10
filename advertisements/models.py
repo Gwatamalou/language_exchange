a86d497a2f49e4e31c01e09d9a0646f6db5d8ce4
Gwatamalou <96253655+Gwatamalou@users.noreply.github.com>
2024-10-10 20:28:03 +0600

    Update models.py

--- advertisements/models.py
+++ advertisements/models.py
@@ -9,10 +9,10 @@ class Advertisement(models.Model):
 
 
 class Notification(models.Model):
-    responder = models.ForeignKey(User, related_name='responder', on_delete=models.CASCADE)  # Пользователь, откликнувшийся на объявление
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Пользователь, которому отправлено уведомление
-    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)  # Соответствующее объявление
-    status = models.CharField(max_length=20, choices=(('pending', 'Pending'), ('accepted', 'Accepted'), ('declined', 'Declined')), default='pending')  # Статус отклика
+    responder = models.ForeignKey(User, related_name='responder', on_delete=models.CASCADE) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=(('pending', 'Pending'), ('accepted', 'Accepted'), ('declined', 'Declined')), default='pending')
 
     def __str__(self):
         return f"Notification for {self.user.username} about {self.advertisement.title} from {self.responder.username}"
