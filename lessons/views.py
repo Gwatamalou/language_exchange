bfc2adb6257fd0bc999abdfe74eaf6f32a51e309
ll <itrebenkov@gmail.com>
2024-10-22 15:31:07 +0600

    базовый чат по вебсокету

--- lessons/views.py
+++ lessons/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def show_conference_view(request):
     return render(request, 'lessons/conference.html',
-                  context={'room_name': 'lesson', 'auth': request.user.id})
+                  context={'room_name': 'lesson', 'auth': request.user.id, 'user_name': request.user.username})
